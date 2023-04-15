ffa7c1ef6a929e3c56132fd2868a3a44c0c56cda
itskahrbaa <7assanelt0p@gmail.com>
2023-04-15 09:05:42 +0200

    new updates

--- app/src/main/java/com/kahrbaa/educat/fragments/Home.kt
+++ app/src/main/java/com/kahrbaa/educat/fragments/Home.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.Nullable
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
 import com.kahrbaa.educat.Course
 import com.kahrbaa.educat.InProgressAdapter
 import com.kahrbaa.educat.R
@@ -16,7 +18,8 @@ import com.kahrbaa.educat.WhoCreated
 
 
 class Home : Fragment() {
-
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var tabLayout: TabLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -28,8 +31,42 @@ class Home : Fragment() {
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_home, container, false)
         val courseData = ArrayList<Course>()
+        recyclerView = rootView.findViewById(R.id.in_progress)
+        tabLayout = rootView.findViewById(R.id.tab_home)
 
-        courseData.add(
+
+        tabs()
+        inProgressFun(rootView, courseData)
+        return rootView
+    }
+
+    private fun tabs() {
+        if (tabLayout.selectedTabPosition == 0) {
+            Toast.makeText(context,"HELLLLLO",Toast.LENGTH_SHORT).show()
+        }
+        tabLayout.addOnTabSelectedListener(object : OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when (tab!!.position) {
+                    0 -> {
+                        Toast.makeText(context,tab!!.text,Toast.LENGTH_SHORT).show()
+                    }
+                    else -> {
+                        println("TTT")
+                    }
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+
+        })
+    }
+
+
+    private fun inProgressFun(rootView: View, data: ArrayList<Course>) {
+
+        data.add(
             Course(
                 "The Complete Dart Learning Guide [2022 Edition]",
                 "A Complete Guide to the Dart Programming Language",
@@ -38,22 +75,15 @@ class Home : Fragment() {
             )
         )
 
-        inProgressFun(rootView, courseData)
-        return rootView
-    }
-
-
-    private fun inProgressFun(rootView: View, data: ArrayList<Course>) {
-
-        val recycler: RecyclerView = rootView.findViewById(R.id.in_progress)
-
         var snapHelper = LinearSnapHelper()
-        snapHelper.attachToRecyclerView(recycler)
+        snapHelper.attachToRecyclerView(recyclerView)
 
-        recycler.layoutManager =
+        recyclerView.layoutManager =
             LinearLayoutManager(rootView.context, LinearLayoutManager.HORIZONTAL, false)
-        recycler.adapter = InProgressAdapter(rootView.context, data)
-        recycler.isNestedScrollingEnabled = false
+        recyclerView.adapter = InProgressAdapter(rootView.context, data)
+        recyclerView.isNestedScrollingEnabled = false
+
+
     }
 
 }
